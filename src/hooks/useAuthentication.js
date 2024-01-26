2cf71175cc1bc15febf71a1303d32bb4f19dbf27
JohNNy <johnnymundi@hotmail.com>
2024-01-26 17:12:04 -0300

    add user register

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,73 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+
+export const useAuthentication = () => {
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
+  // cleanup
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  const auth = getAuth();
+
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
+    }
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "E-mail já cadastrado.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor, tente mais tarde.";
+      }
+
+      setLoading(false);
+    }
+  };
+
+  // para colocar o cancelado com true assim que sair da página
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { db } from "./firebase/config";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const database = db; // tem que importar o db para não dar problemas
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
